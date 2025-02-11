e92e926defa93fbe8be322f73072a697d70a50d6
meziane228@gmail.com <meziane228@gmail.com>
2025-02-11 17:54:17 -0500

    First Commit here

---
+++ src/main/java/com_Java_Try_Again/LetsCode.java
@@ -0,0 +1,16 @@
+package com_Java_Try_Again;
+
+public class LetsCode {
+	
+	public static void main (String[] arg) {
+		//System.out.println("Hello world");
+		int a = 3;
+		int b = 4;
+		int x=a*b;	
+		//System.out.println(a+b);
+		//System.out.println(a-b);
+		//System.out.println(b-a);
+		System.out.println(x);
+	}
+
+}
---
+++ src/test/java/com/Try_Testing_Again/DropDown_Exercice.java
@@ -0,0 +1,55 @@
+package com.Try_Testing_Again;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DropDown_Exercice {
+	public DropDown_Exercice() throws Exception {
+		super();
+	}
+	WebDriver driver;
+	
+	
+
+
+	public static void main (String[] args) {
+		
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window();
+		//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));		
+		//WebElement createNewAccountButton = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Create new account")));
+		//createNewAccountButton.click();
+		WebElement createNewAccountButton = driver.findElement(By.linkText("Create new account"));		
+		createNewAccountButton.click();
+		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Aziz");
+		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Boutef");
+		Select monthDropdown = new Select(driver.findElement(By.id("month")));
+		Select dayDropdown = new Select(driver.findElement(By.id("day")));
+		Select yearDropdown = new Select(driver.findElement(By.id("year")));
+		monthDropdown.selectByValue("10");
+		dayDropdown.selectByValue("19");
+		yearDropdown.selectByValue("1973");
+		driver.findElement(By.xpath("//input[@value='2']")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement phoneField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='reg_email__']")));
+		phoneField.sendKeys("510 546 0743");
+		driver.findElement(By.xpath("//input[@id='password_step_input' and @type='password']")).sendKeys("USA2025");
+		WebDriverWait waitt = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement signUp = waitt.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Sign Up']")));
+		signUp.click();
+		driver.findElement(By.xpath("//button[@type='submit' and @id='u_0_n_9s']")).click();
+	}
+	public void tearDown() {
+		
+		driver.quit();
+	}
+}
